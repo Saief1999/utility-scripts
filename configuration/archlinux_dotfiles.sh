e9045a0949d5c47c1c58bcee04365bd9bff8d56e
Saief Zneti <saief_zaneti@yahoo.com>
2023-06-10 10:57:59 +0200

    Additions to utility scripts

--- configuration/archlinux_dotfiles.sh
+++ configuration/archlinux_dotfiles.sh
@@ -5,7 +5,9 @@
 # Extra setup : Dotfiles
 readonly dotfiles_path="$(pwd)/../dotfiles"
 
-ln -sf $dotfiles_path/.translate-shell $HOME/
-ln -sf $dotfiles_path/.system $HOME/
+ln -sf "$dotfiles_path/.translate-shell" "$HOME/"
+ln -sf "$dotfiles_path"/.system "$HOME/"
+ln -sf "$dotfiles_path/.kitty" "$HOME/.config/kitty/kitty.conf"
 
+# This part should be executed only onces
 #echo 'source $HOME/.system/.init' >> $HOME/.bashrc
